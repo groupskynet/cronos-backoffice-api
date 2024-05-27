47d7481dc9e0f57dcd0d5417ce8d242258251e27
camilo <colonca1999@gmail.com>
2024-05-27 06:29:25 -0500

    feat: diod controllers dynamics

--- src/contexts/shared/infrastructure/dependency_injection/diod.config.ts
+++ src/contexts/shared/infrastructure/dependency_injection/diod.config.ts
@@ -1,12 +1,24 @@
+import { ContainerBuilder } from 'diod'
+import { glob } from 'glob'
+
 import { DynamodbGameRepository } from '@contexts/games/infrastructure/DynamodbGameRepository'
 import { CreateNewGame } from '@contexts/games/application/CreateNewGame'
-import { ContainerBuilder } from 'diod'
 import { EventBus } from '../../domain/event/EventBus'
 import { AwsEventBridgeEventBus } from '../event_bus/AwsEventBridgeEventBus'
 import { GameRepository } from '@src/contexts/games/domain/contracts/GameRepository'
 import { CreateNewGameOnBallsGenerated } from '@src/contexts/games/application/domain_events/CreateNewGameOnBallsGenerate'
 
 const builder = new ContainerBuilder()
+
+const controllers = glob.sync(`src/**/*Controller.ts`)
+
+controllers.map((controller) => {
+	const match = controller.match(/\/([^/]+)\.ts$/)
+	const className = match ? match[1] : ''
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const object = require(controller)
+	builder.registerAndUse(object[className])
+})
 
 builder.register(GameRepository).use(DynamodbGameRepository)
 
