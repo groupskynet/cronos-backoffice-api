348cab1de33013c242929ccc92d0aebc8a43ab9f
camilo <colonca1999@gmail.com>
2024-05-29 00:29:08 -0500

    feat: update game pending by play

--- src/api/controllers/UpdateGameController.ts
+++ src/api/controllers/UpdateGameController.ts
@@ -1,12 +1,15 @@
-import { GameRepository } from '@contexts/games/domain/contracts/GameRepository'
-import { Controller, Put, Path, Route, SuccessResponse } from 'tsoa'
+import { UpdatePendingGameToPlayService } from '@contexts/games/application/UpdatePendingGameToPlayService'
+import { Controller, Put, Path, Route, SuccessResponse, Body } from 'tsoa'
 
 @Route('game')
 export class UpdateGameController extends Controller {
-
-  constructor(private readonly service: Update) {}
+  constructor(private readonly service: UpdatePendingGameToPlayService) {
+    super()
+  }
 
   @Put('update/{id}')
   @SuccessResponse('200', 'Updated')
-  async run(@Path() id: string): Promise<void> {}
+  async run(@Path() id: string, @Body() body: { balls: number[] }): Promise<void> {
+    this.service.run(id, body.balls)
+  }
 }
