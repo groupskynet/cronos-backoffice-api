4cf36dbace6413db1ecf55afece5eb0121e99099
camilo <colonca1999@gmail.com>
2024-05-29 00:39:08 -0500

    feat: update game pending by play

--- src/contexts/shared/infrastructure/dependency_injection/diod.config.ts
+++ src/contexts/shared/infrastructure/dependency_injection/diod.config.ts
@@ -7,23 +7,25 @@ import { CreateNewGameOnBallsGenerated } from '@src/contexts/games/application/d
 import { GameRepository } from '@contexts/games/domain/contracts/GameRepository'
 import { DynamodbGameRepository } from '@contexts/games/infrastructure/DynamodbGameRepository'
 import { CreateNewGame } from '@contexts/games/application/CreateNewGame'
+import { UpdatePendingGameToPlayService } from '@contexts/games/application/UpdatePendingGameToPlayService'
 
 const builder = new ContainerBuilder()
 
 const controllers = glob.sync(`src/**/*Controller.ts`)
 
 controllers.map((controller) => {
-	const match = controller.match(/\/([^/]+)\.ts$/)
-	const className = match ? match[1] : ''
-	// eslint-disable-next-line @typescript-eslint/no-var-requires
-	const object = require(controller)
-	return builder.registerAndUse(object[className]).asSingleton()
+  const match = controller.match(/\/([^/]+)\.ts$/)
+  const className = match ? match[1] : ''
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const object = require(controller)
+  return builder.registerAndUse(object[className]).asSingleton()
 })
 
 builder.register(GameRepository).use(DynamodbGameRepository)
 
 builder.registerAndUse(CreateNewGame)
 builder.registerAndUse(CreateNewGameOnBallsGenerated).addTag('subscriber')
+builder.registerAndUse(UpdatePendingGameToPlayService)
 
 builder.register(EventBus).use(AwsEventBridgeEventBus)
 
