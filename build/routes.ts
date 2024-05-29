5483d7947832babe0774190b9002c6cf75800893
camilo <colonca1999@gmail.com>
2024-05-29 00:33:13 -0500

    feat: update game pending by play

--- build/routes.ts
+++ build/routes.ts
@@ -34,6 +34,7 @@ export function RegisterRoutes(app: Router) {
             async function UpdateGameController_run(request: ExRequest, response: ExResponse, next: any) {
             const args: Record<string, TsoaRoute.ParameterSchema> = {
                     id: {"in":"path","name":"id","required":true,"dataType":"string"},
+                    body: {"in":"body","name":"body","required":true,"dataType":"nestedObjectLiteral","nestedProperties":{"balls":{"dataType":"array","array":{"dataType":"double"},"required":true}}},
             };
 
             // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
--- tests/units/games/__mocks__/GameRepositoryMock.ts
+++
@@ -1,9 +0,0 @@
-import { Game } from '@contexts/games/domain/Game'
-import { GameRepository } from '@contexts/games/domain/contracts/GameRepository'
-
-export class GameRepositoryMock extends GameRepository {
-  private readonly mock = jest.fn()
-  async save(game: Game): Promise<void> {
-    await this.mock(game)
-  }
-}
