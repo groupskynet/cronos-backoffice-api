c670fdd41f63a5a72d3cc0bd434a6c9fb0ef4684
Gerson Moreno <gmorenoc@MacBook-Pro-de-Gerson.local>
2024-06-28 13:39:33 -0500

    feat: se agrego el archivo .env

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,9 +1,11 @@
 import { Server } from './server'
+import * as dotenv from 'dotenv'
 
 export class Bootstrap {
 	server: Server
 
 	constructor() {
+		dotenv.config()
 		const port = process.env.port ?? '3005'
 		this.server = new Server(port)
 	}
